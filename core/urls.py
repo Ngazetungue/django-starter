26961f69a096af3ea0d13441d00b9b307d68d79e
Ngazetungue <nmuheuez@gmail.com>
2025-05-18 06:02:14 +0200

    first commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+# core/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
+]
